eed5e6f822cb557406e045caa58a5bc614d46edd
kartik589123 <meta619012@gmail.com>
2023-08-31 19:17:27 +0530

    Added virtuals

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const connect = require("./config/database");
 const app = express();
+const { PORT } = require("./config/serverconfig");
 
 const TweetRepository = require("./repository/tweet-repository");
 const Comment = require("./models/comment");
 
-app.listen(3000, async () => {
-  console.log("server started");
+app.listen(PORT, async () => {
+  console.log(`server started at ${PORT}`);
+
   await connect();
   console.log("Mongodb connected");
 
@@ -15,6 +17,6 @@ app.listen(3000, async () => {
   // });
   //const tweets = await Tweet.find({ userEmail: null });
   const tweetrepo = await new TweetRepository();
-  const tweet = await tweetrepo.getWithComment("64f05872db0b2bd81ab67907");
-  console.log(tweet);
+  const tweet = await tweetrepo.getAll(0, 1);
+  console.log(tweet[0].contentWithEmail);
 });
